ec0fa2bd3a29b3fd22e6c02cf91c4c76a26e362e
Kyjko <bognarmiklos34@gmail.com>
2021-05-18 14:00:28 +0200

    errors

--- deficit_model.py
+++ deficit_model.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 class DeficitModel:
     def __init__(self, gamma: float, tau: float, delta0: float, r: float, g: float):
         self.__gamma: float = gamma
@@ -6,6 +9,8 @@ class DeficitModel:
         self.__r: float = r
         self.__g: float = g
         self.deltas: [float] = [self.__delta0]
+        self.__error: str = ""
+
 
     def __str__(self):
         os: str = "Growth: {}\nInitial debt: {}\nTaxes: {}\nInterest rate: {}\nGovernment spending: {}\n".\
@@ -22,6 +27,13 @@ class DeficitModel:
         for i in range(n):
             delta = self.deltas[-1]*(1+self.__r)/(1+self.__g) + (self.__gamma-self.__tau)/(1+self.__g)
             self.deltas.append(delta)
+
+    def stable(self) -> Union[float, str]:
+        try:
+            return (self.__tau - self.__gamma) / (self.__r - self.__g)
+        except ZeroDivisionError:
+            self.__error = "Cannot determine stable delta!"
+            return "{} > {}".format(hash((self.__error, 1)), self.__error)
 
     def print(self) -> None:
         os = "Deltas:\n"
